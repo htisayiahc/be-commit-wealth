d7d838204f582b3aced2ebc06823993d71d4a603
htisayiahc <a5830050a@gmail.com>
2026-09-11 10:37:24 +0700

    add constrain user duplication

--- src/main/java/be/commit_wealth/repository/UserRepository.java
+++ src/main/java/be/commit_wealth/repository/UserRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+
     // Custom query method generated automatically by Spring Data
     Optional<User> findByEmail(String email);
 }
--- src/main/java/be/commit_wealth/services/UserService.java
+++ src/main/java/be/commit_wealth/services/UserService.java
@@ -1,10 +1,13 @@
 package be.commit_wealth.services;
 
+import be.commit_wealth.constants.ConstantValue;
 import be.commit_wealth.dto.UserRegistrationRequest;
 import be.commit_wealth.model.User;
 import be.commit_wealth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,16 @@ public class UserService {
     private final UserRepository userRepository;
 
     public User registerUser(UserRegistrationRequest request) {
+        if (userRepository.findByUsername(request.getUsername()).isPresent()) {
+            throw new RuntimeException(ConstantValue.USERNAME_EXISTS_ERROR_MESSAGE);
+        }
+
+        if (userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RuntimeException(ConstantValue.EMAIL_EXISTS_ERROR_MESSAGE);
+        }
+
         User user = new User(request);
+        user.setId(UUID.randomUUID());
         return userRepository.save(user);
     }
 }
